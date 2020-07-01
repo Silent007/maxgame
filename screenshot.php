450e87f26dd68e3888455e5888612ade35b6982c
Paswan abhijeet kumar <66725774+Silent007@users.noreply.github.com>
2020-07-01 17:59:33 +0530

    Add files via upload

---
+++ screenshot.php
@@ -0,0 +1,277 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <!-- Socialmedia Icon   -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            list-style: none;
+        }
+
+        .footer {
+            display: flex;
+            text-align: center;
+            align-items: center;
+            justify-content: center;
+
+        }
+
+        #foot {
+            display: flex;
+        }
+
+        #foot li {
+            position: relative;
+            display: block;
+            color: #666;
+            font-size: 30px;
+            height: 60px;
+            width: 60px;
+            background: #171515;
+            line-height: 60px;
+            border-radius: 50%;
+            margin: 29px 22px 8px 8px;
+            ;
+            cursor: pointer;
+            transition: .5s;
+        }
+
+        #foot li:before {
+            position: absolute;
+            content: '';
+            top: 0;
+            left: 0;
+            background: #d35400;
+            height: 100%;
+            width: 100%;
+            border-radius: 50%;
+            z-index: -1;
+            transform: scale(0.9);
+        }
+
+        #foot li:hover::before {
+            filter: blur(3px);
+            transform: scale(1.2);
+            box-shadow: 0 0 15px #d35400;
+        }
+
+        #foot li:hover {
+            color: #ffa502;
+            box-shadow: 0 0 15px #d35400;
+            text-shadow: 0 0 15px #d35400;
+
+        }
+    </style>
+
+    <title>X-Men Origins: Wolverine Free Download</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="max.php">
+            <img src="logo.png" width="100" height="60" class="d-inline-block align-top" alt="" loading="lazy">
+        </a>
+        <h1 class="text-primary"> Max <span class="badge badge-secondary">New</span></h1>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="max.php">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">ABOUT</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="login.php">Login</a>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        GAMES
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="action.php">ACTION GAMES</a>
+                        <a class="dropdown-item" href="#">RACES GAMES</a>
+                        <a class="dropdown-item" href="#">SPORTS GAMES</a>
+                        <a class="dropdown-item" href="#">PUZZELS GAMES</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">CATEGROY GAMES</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+    <div class="p-3 mb-2 bg-dark text-white">
+
+        <section>
+            <div class="card text-center">
+                <div class="card-header">
+                    <h2 class="text-body">About the Game</h2>
+                </div>
+                <div class="card-body">
+                    <h3 class="text-body">X-Men Origins: Wolverine Free Download</h3>
+                    <p class="text-body">X-Men Origins: Wolverine Free Download PC Game pre-installed in direct link.
+                        X-Men Origins: Wolverine was released on May 1, 2009</p>
+                    <br>
+                    <p class="text-body">The X-Men saga continues with the X-Men Origins: Wolverine video game for the
+                        PC, based on the
+                        feature film of the same name. Unleash the fury of Wolverine and see how it all began. With
+                        cinema-style graphics and sound, and unstoppable action and excitement, X-Men Origins: Wolverine
+                        will have you on the edge of seat as you slash, slice, and stab your way through level after
+                        level of intense, epic gameplay. With incredibly detailed graphics and stellar sound effects,
+                        you’ll feel as though you’re watching a full-feature movie, only you’ll be playing it on your
+                        PC. X-Men Origins: Wolverine takes you back to the “birth” of Wolverine when the fury was first
+                        unleashed. Battle your way through levels of relentless action that feature jaw-dropping
+                        graphics that are suitable enough for the big screen. Take on the enemy as only Wolverine can
+                        using his indestructible adamantium claws to defeat even the toughest adversaries. With over 100
+                        custom moves and attacks, reflex quick-kills, and long-range lunge attacks, you have enough
+                        destruction power at your fingertips for even the most intense combat situations.</p>
+                    <ul class="text-body" style="list-style-type:circle;">
+
+                        <li> &#8226; Cinema quality action sequences with jaw-dropping graphics and detail</li>
+                        <li> &#8226; 100 custom moves, reflex quick-kills, and long-range lunge attacks
+                            Rage abilities to unlock special moves and powers</li>
+                        <li> &#8226; Utilize Wolverine’s Feral Senses for tactical advantages over the enemy
+                            Real-time regeneration powers to mend bones and tissues for non-stop combat action</li>
+                    </ul>
+                    <div class="card bg-dark text-white">
+                        <img src="x1.jpg" height="366" width="650" sizes="(max-width: 650px) 100vw, 650px"
+                            class="card-img" alt="...">
+                        <div class="card-img-overlay">
+                        </div>
+                    </div>
+                    <br>
+                    <h2 class="text-body">How to Download & Install X-Men Origins: Wolverine</h2>
+                    <p>
+                    <ol class="text-body" type="1">
+                        <li>&#8227; Click the Download button below and you should be redirected to maxGamer.</li>
+                        <li>&#8227; click on the blue ‘download now’ button. Now let the download begin and
+                            wait for it to finish</li>
+                        <li>&#8227; Once X-Men Origins: Wolverine is done downloading, right click the .zip file and
+                            click on
+                            “Extract to X-Men.Origins.Wolverine.zip” (To do this you must have WinRAR, which you can
+                            get <span style="font-weight: bold;">here</span>).</li>
+                        <li>&#8227; Double click inside the X-Men Origins: Wolverine folder and run the exe application.
+                        </li>
+                        <li> &#8227; Have fun and play! Make sure to run the game as administrator and if you get any
+                            missing dll
+                            errors, look for a Redist or _CommonRedist folder and install all the programs in the
+                            folder.</li>
+                    </ol>
+                    </p>
+                    <br>
+                    <h3 class="text-body">X-Men Origins: Wolverine Free Download</h3>
+                    <p class="text-body">
+                        Click the download button below to start X-Men Origins: Wolverine Free Download with direct
+                        link. It is the full version of the game. Don’t forget to run the game as administrator.
+                    </p>
+                    <a href="X-Men.Origins.Wolverine.zip" class="btn btn-primary">X-Men Origins: Wolverine <p>
+                            Size:6.07GB</p></a>
+                    <br>
+                    <p class="text-body"><span style="color: red;" style="font-family: cursive;">NOTICE:</span> This
+                        game is already pre-installed for you, meaning you don’t have to install it. If you
+                        get any missing dll errors, make sure to look for a _Redist or _CommonRedist folder and install
+                        directx, vcredist and all other programs in that folder. You need these programs for the game to
+                        run. Look for a ‘HOW TO RUN GAME!!.txt’ file for more help. Also, be sure to right click the exe
+                        and always select “Run as administrator” if you’re having problems saving the game. Always
+                        disable your anti virus before extracting the game to prevent it from deleting the crack files.
+                        If you need additional help, click here
+
+                    </p>
+                    <br>
+                    <h3 class="text-body"> System Requirements</h3>
+                    <p>
+                        <li class="text-body"><span style="font-weight: bolder;"> Supported OS:</span> Win Xp 32 and 64.</li>
+                        <li class="text-body"><span style="font-weight: bolder ;">Processor:</span> Intel Pentium 4 3.0GHz / AMD Athlon 64 3000+.</li>
+                        <li class="text-body"><span style="font-weight: bolder;">Memory:</span> 1 GB</li>
+                        <li class="text-body"><span style="font-weight: bolder;">Graphics:</span> AMD Radeon X1800 Series 256MB or NVIDIA GeForce 7800 GT.</li>
+                        <li class="text-body"><span style="font-weight: bolder;">Drivers:</span> DirectX® 9.0c (included) and latest video drivers</li>
+                        <li class="text-body"><span style="font-weight: bolder;">Hard Drive:</span>     6 GB free space.</li>
+                        <li class="text-body"><span style="font-weight: bolder;">Sound:</span> DirectX 9.0c compatible sound card<li></li>
+                        </p>
+                        <br>
+                        <h3 class="text-body">Screenshots</h3>
+                        <div class="card bg-dark text-white">
+                            <img src="x2.jpg" height="366" width="650" sizes="(max-width: 650px) 100vw, 650px"
+                                class="card-img" alt="...">
+                            <div class="card-img-overlay">
+                            </div>
+                            <br>
+                            <div class="card bg-dark text-white">
+                                <img src="x3.jpg" height="366" width="650" sizes="(max-width: 650px) 100vw, 650px"
+                                    class="card-img" alt="...">
+                                <div class="card-img-overlay">
+                                </div>
+                                <br>
+                                <div class="card bg-dark text-white">
+                                    <img src="x4.jpg" height="366" width="650" sizes="(max-width: 650px) 100vw, 650px"
+                                        class="card-img" alt="...">
+                                    <div class="card-img-overlay">
+                                    </div>
+                                    <br>
+                    
+                </div>
+                <div class="card-footer text-muted">
+                </div>
+            </div>
+        </section>
+
+        <!-- Footer -->
+
+        <footer class="footer">
+            <div>
+                <ul id="foot">
+                    <li><a href="https://www.facebook.com/profile.php?id=100010939177189" target="_blank"><i
+                                class="fa fa-facebook"></i></a></li>
+                    <li><a href="https://www.instagram.com/paswan_abhijeet/" target="_blank"><i
+                                class="fa fa-instagram"></i></a>
+                    </li>
+                    <li><a href="https://www.linkedin.com/in/paswan-abhijeet-kumar-0074471a2/" target="_blank"><i
+                                class="fa fa-linkedin"></i></a> </li>
+                </ul>
+
+                <p class="footer-copyright text-center py-3">
+                    <a href="https://maxgamer.com"> Maxgamer.com</a>
+                </p>
+            </div>
+
+        </footer>
+
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+            crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+            crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+            integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
+            crossorigin="anonymous"></script>
+    </div>
+    </div>
+</body>
+
+
+</html>
